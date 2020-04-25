dfd3627bc93f84aceb6cbebec228be0f8934ca3f
Alex <chenchenalex1@gmail.com>
2020-04-25 12:29:39 +1000

    add comments to cluster js

--- nodejs/server-availability/cluster.js
+++ nodejs/server-availability/cluster.js
@@ -1,3 +1,13 @@
+// instruction
+/* 
+1. start the server with node cluster.js
+2. server logs the master pid, get read with command kill -SIGUSR2 {pid}
+3. start monitoring the server by using ab -c200 -t10 http://localhost:8080/
+4. kill the process on step 2, server will restart itself one by one
+5. observe the result in the benchmark, see if any request is failed
+6. and give a try to process manager like PM2 https://github.com/Unitech/pm2
+*/
+
 const cluster = require("cluster");
 const os = require("os");
 
