609bc706f084c58b42d67ab34b2a34142ba0c95f
Alex <chenchenalex1@gmail.com>
2020-06-11 20:08:47 +1000

    composite components

---
+++ taskbox-storybook-tutorial/src/components/TaskList.js
@@ -0,0 +1,78 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Task from "./Task";
+
+function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+  const events = {
+    onPinTask,
+    onArchiveTask,
+  };
+
+  const LoadingRow = (
+    <div className="loading-item">
+      <span className="glow-checkbox" />
+      <span className="glow-text">
+        <span>Loading</span> <span>cool</span> <span>state</span>
+      </span>
+    </div>
+  );
+
+  if (loading) {
+    return (
+      <div className="list-items">
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+      </div>
+    );
+  }
+
+  if (tasks.length === 0) {
+    return (
+      <div className="list-items">
+        <div className="wrapper-message">
+          <span className="icon-check" />
+          <div className="title-message">You have no tasks</div>
+          <div className="subtitle-message">Sit back and relax</div>
+        </div>
+      </div>
+    );
+  }
+
+  const pinnedTasks = [];
+  const regularTasks = [];
+
+  tasks.forEach((task) => {
+    if (task.state === "TASK_PINNED") {
+      pinnedTasks.push(task);
+    } else {
+      regularTasks.push(task);
+    }
+  });
+
+  const sortedTasks = [...pinnedTasks, ...regularTasks];
+
+  return (
+    <div className="list-items">
+      {sortedTasks.map((task) => (
+        <Task key={task.id} task={task} {...events} />
+      ))}
+    </div>
+  );
+}
+
+TaskList.propTypes = {
+  loading: PropTypes.bool,
+  tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired, // import Task.propTypes.task
+  onPinTask: PropTypes.func.isRequired,
+  onArchiveTask: PropTypes.func.isRequired,
+};
+
+TaskList.defaultProps = {
+  loading: false,
+};
+
+export default TaskList;
