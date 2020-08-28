50e4e8af12432699328b69f7bed2898994331b72
Chen, N. A <n.a.chen@accenture.com>
2020-08-28 12:02:53 +1000

    set up graphql server and decorate Post with type-graphql

--- full-stack-tutorial/src/index.ts
+++ full-stack-tutorial/src/index.ts
@@ -1,16 +1,36 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entity/Post";
 import microConfigs from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfigs);
   await orm.getMigrator().up();
-  // const post = orm.em.create(Post, { title: "my first post" });
-  // await orm.em.persistAndFlush(post);
 
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  const app = express();
+
+  app.listen(4000, () => {
+    console.log("server started at localhost:4000");
+  });
+
+  app.get("/", (_, res) => {
+    res.send("hi world");
+  });
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
 };
 
 main().catch((e) => {
---
+++ full-stack-tutorial/src/resolvers/post.ts
@@ -0,0 +1,16 @@
+import { Resolver, Query, Ctx, Arg, Int } from "type-graphql";
+import { Post } from "../entity/Post";
+import { MyContext } from "../types";
+
+@Resolver()
+export class PostResolver {
+  @Query(() => [Post])
+  posts(@Ctx() ctx: MyContext): Promise<Post[]> {
+    return ctx.em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(@Arg("id", () => Int) id: number, @Ctx() ctx: MyContext): Promise<Post | null> {
+    return ctx.em.findOne(Post, { id });
+  }
+}
