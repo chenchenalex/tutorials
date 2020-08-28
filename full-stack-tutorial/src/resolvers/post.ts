9576e8910143bd9a3e149067bd6b41db5d594622
Chen, N. A <n.a.chen@accenture.com>
2020-08-28 16:09:13 +1000

    add CRUD operations to post

--- full-stack-tutorial/src/resolvers/post.ts
+++ full-stack-tutorial/src/resolvers/post.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Ctx, Arg, Int } from "type-graphql";
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from "type-graphql";
 import { Post } from "../entity/Post";
 import { MyContext } from "../types";
 
@@ -12,5 +12,42 @@ export class PostResolver {
   @Query(() => Post, { nullable: true })
   post(@Arg("id", () => Int) id: number, @Ctx() ctx: MyContext): Promise<Post | null> {
     return ctx.em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(@Arg("title") title: string, @Ctx() { em }: MyContext): Promise<Post> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg("id") id: number,
+    @Arg("title", { nullable: true }) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { id });
+    if (!post) {
+      return null;
+    }
+
+    if (typeof title !== "undefined") {
+      post.title = title;
+      await em.persistAndFlush(post);
+    }
+
+    return post;
+  }
+
+  @Mutation(() => Boolean, { nullable: true })
+  async deletePost(@Arg("id") id: number, @Ctx() { em }: MyContext): Promise<boolean> {
+    try {
+      await em.nativeDelete(Post, { id });
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
 }
