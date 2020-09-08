ae1d5113d4d5f970bfd8f8465385fef33d328c25
Chen, N. A <n.a.chen@accenture.com>
2020-09-08 10:32:37 +1000

    create query builder for custom registry process

--- full-stack-server/src/resolvers/user.ts
+++ full-stack-server/src/resolvers/user.ts
@@ -2,6 +2,7 @@ import { Resolver, InputType, Field, Mutation, Arg, Ctx, ObjectType, Query } fro
 import { MyContext } from "src/types";
 import { User } from "../entity/User";
 import argon2 from "argon2";
+import { EntityManager } from "@mikro-orm/postgresql";
 
 @InputType()
 class UserNamePasswordInput {
@@ -68,15 +69,23 @@ export class UserResolver {
       };
     }
 
-    const user = em.create(User, {
-      username: options.username,
-      password: hashedPassword,
-    });
-
+    let user;
     try {
-      await em.persistAndFlush(user);
+      // query builder
+      const result = await (em as EntityManager)
+        .createQueryBuilder(User)
+        .getKnexQuery()
+        .insert({
+          username: options.username,
+          password: hashedPassword,
+          created_at: new Date(),
+          updated_at: new Date(),
+        })
+        .returning("*");
+
+      user = result[0];
     } catch (err) {
-      if (err.code === "23505") {
+      if (err.detail.includes("already exists")) {
         // duplicate user error
         return {
           errors: [
