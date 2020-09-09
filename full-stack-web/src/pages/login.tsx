2db49b6d9cc314d2fc77b6fdc1b4758d6de2bd1a
Chen, N. A <n.a.chen@accenture.com>
2020-09-09 10:00:01 +1000

    add login page and graphql

---
+++ full-stack-web/src/pages/login.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import { Box, Button } from "@chakra-ui/core";
+import Wrapper from "../components/Wrapper";
+import InputField from "../components/InputField";
+import { useLoginMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
+
+const Login: React.FC<{}> = ({}) => {
+  const router = useRouter();
+  const [, login] = useLoginMutation();
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await login({ options: values });
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data?.login.user) {
+            router.push("/");
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField name="username" placeholder="username" label="Username" />
+            <Box>
+              <InputField name="password" placeholder="password" label="Password" type="password" />
+            </Box>
+            <Button type="submit" isLoading={isSubmitting} variantColor="teal" mt={4}>
+              Login
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Login;
